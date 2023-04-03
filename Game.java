4f8dfa594154e8844aac3b2c4792d3d993b67d60
Michael Cummings <mcummings7@worcester.edu>
2023-04-03 16:24:44 -0400

    Ninth commit, rewrote createMovableGameObjectObstacle and gameTimePulse, added KeyListener object in gameInitialize

--- Game.java
+++ Game.java
@@ -13,32 +13,43 @@ import javax.swing.JPanel;
  * A class representing an instance of a mobile game
  *
  * @author Michael Cummings
- * @version 3.30.23
+ * @version 4.3.23
  */
-public class Game
+public class Game extends JFrame
 {
     private boolean gameOver;
     private JPanel gameGraphics;
     private KeyListener gameInput;
-    public static int gameWidth = 480;
-    public static int gameHeight = 854;//9:16 480p aspect ratio
+    public static final int gameWidth = 480;
+    public static final int gameHeight = 854;//9:16 480p aspect ratio
     public String gameTitle = "WorkingTitle";
     private boolean[] keyboard;
     private ArrayList<MovableGameObject> gameWorldObjects;
     private Player gamePlayerCharacter;
+    private int gamePlayerCharacterStartXCoordinate = gameWidth / 2;
+    private int gamePlayerCharacterStartYCoordinate = gameHeight + 16;
     private long gamePointsScore;
     private long gamePointsHighScore = 0;
     //private int gameTimeCounter; Used for adjusting difficulty, can come back to this later
     private long gameTimeDelay; //Need this for graphics rendering code
-    private Random gameRandomSeed; //Will hold a random value generated at initialization that will be used to decide whether the game creates an obstacle on the play screen, also will be used to determine object dimensions
+    private Random gameRandomSeed; //Will hold a random value generated at initialization that will be used to decide the coordinates to place Obstacles
     
     /**
      * Method to instantiate an Obstacle MovableGameObject
-     * Intended to make Obstacles appear at a random point along the x-axis, and at a random point above 0 on the y-axis to give the player time to react as it moves down the screen and up the y-axis
+     * Intended to make Obstacles appear at a random point along the x-axis from 16 to 464, and at a random point from -32 to -886 on the y-axis to give the player time to react as it moves down the screen and up the y-axis
      */
     public Obstacle createMovableGameObjectObstacle()
     {
-        return new Obstacle((int)(gameRandomSeed.nextDouble() * (gameWidth + 32)) - 16, (int)(gameRandomSeed.nextDouble() * (-gameHeight + 32)));
+        return new Obstacle((int)(gameRandomSeed.nextDouble() * (gameWidth + 16)) - 32, (int)(gameRandomSeed.nextDouble() * -gameHeight - 32));
+    }
+    
+    /**
+     * Method to instantiate a Player MovableGameObject
+     * @param x, y
+     */
+    public Player createMovableGameObjectPlayer()
+    {
+        return new Player(gamePlayerCharacterStartXCoordinate, gamePlayerCharacterStartYCoordinate);
     }
     
     /**
@@ -47,18 +58,33 @@ public class Game
     public void gameInitialize()
     {
         gameOver = false;
+        
         keyboard = new boolean[KeyEvent.KEY_LAST]; 
-        gamePlayerCharacter = new Player(gameWidth / 2, gameHeight - 24); //Values subject to change once I see how the game looks on a phone screen
+        gameInput = new KeyListener()
+        {
+            public void keyTyped(KeyEvent key)
+            {
+            }
+            public void keyPressed(KeyEvent key)
+            {
+                keyboard[key.getKeyCode()] = true;
+            }
+            public void keyReleased(KeyEvent key)
+            {
+                keyboard[key.getKeyCode()] = false;
+            }
+        };
+        
+        gamePlayerCharacter = createMovableGameObjectPlayer(); //Values subject to change once I see how the game looks on a phone screen
         gamePointsScore = 0;
         gameRandomSeed = new Random((long)(Math.random() * Long.MAX_VALUE));
         gameWorldObjects = new ArrayList<>();
         
-        for (int i = 0; i < gameWidth / 5; i++)
+        
+        for (int i = 0; i < 10; i++) //maybe create method later to produce a value for i to count up to for more Obstacles to appear at once as the player keeps winning the game? Function of game time and/or points
         {
             gameWorldObjects.add(createMovableGameObjectObstacle()); //Have only created one type of Obstacle for now, maybe rewrite to call a dedicated MovableObject spawner method that can add different types of Obstacles (regular, large, fast, etc.)
         }
-        
-        //In progress
     }
     
     /**
@@ -68,23 +94,23 @@ public class Game
     {
         if(gameOver)
         {
-            if(keyboard[KeyEvent.VK_SPACE]) //Lets the player start a new game from the gameover state
+            if(keyboard[KeyEvent.VK_SPACE]) //Lets the player start a new game from the gameOver state
             {
                 gameInitialize();
             }
             return;
         }
         
-        if(keyboard[KeyEvent.VK_A] || keyboard[KeyEvent.VK_LEFT])
+        if(keyboard[KeyEvent.VK_A] || keyboard[KeyEvent.VK_LEFT]) //player should be able to move from [8,838] on the x-axis to keep entire Player object in screen space
         {
-            if(!(gamePlayerCharacter.getGameObjectLocation().getX() - gamePlayerCharacter.getGameObjectWidth() / 2 <= 0))
+            if(gamePlayerCharacter.getGameObjectLocation().getX() - gamePlayerCharacter.getGameObjectLeftXCoordinate() > 0)
             {
                 gamePlayerCharacter.translateMovableGameObject((int)gamePlayerCharacter.getMovableGameObjectSpeed() * -1, 0);
             }
         }
         if(keyboard[KeyEvent.VK_D] || keyboard[KeyEvent.VK_RIGHT])
         {
-            if(!(gamePlayerCharacter.getGameObjectLocation().getX() + gamePlayerCharacter.getGameObjectWidth() / 2 >= gameWidth))
+            if(gamePlayerCharacter.getGameObjectLocation().getX() + gamePlayerCharacter.getGameObjectRightXCoordinate() < gameWidth)
             {
                 gamePlayerCharacter.translateMovableGameObject((int)gamePlayerCharacter.getMovableGameObjectSpeed(), 0);
             }
@@ -118,6 +144,10 @@ public class Game
             }
         }
     }
+    
+    /**
+     * TODO: Graphics rendering method
+     */
 
     /**
      * Constructor for objects of class Game
@@ -125,6 +155,8 @@ public class Game
     public Game()
     {
         Game newGame = new Game();
+        newGame.setVisible(false);
+        newGame.setSize(gameWidth, gameHeight);
         
         //TODO: Create setup method to initialize KeyListener, Graphics and JFrame, implement GameTimePulse
         //Create driver / main method
